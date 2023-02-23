9a32922be65bc9455cd71d326d8e6fe45aac7258
山西清水欧度信息技术有限公司 <lin.jun@odooqs.com>
2023-02-23 14:52:45 +0800

    tst

---
+++ test/models/mail_thread.py
@@ -0,0 +1,17 @@
+from odoo import models, tools
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class MailThread(models.Model):
+    _inherit = 'mail.thread'
+
+    def message_process(self, model, message, custom_values=None,
+                        save_original=False, strip_attachments=False,
+                        thread_id=None):
+        _logger.info('*************')
+        _logger.info(self.env.user.id)
+        _logger.info('********')
+        return super(MailThread, self).message_process(model, message, custom_values,
+                                                       save_original, strip_attachments, thread_id)
